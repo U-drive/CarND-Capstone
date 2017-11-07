07ea75cf45cbdb422dcc9ff37d13e6728516f0c9
Andrew Dunk <drew.dunk@holovis.com>
2017-11-07 15:33:54 -0800

    imported the supplied controllers and filter into the controller class and created and instance of each. some ? that need filling in when i figure out what they should be.

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -1,4 +1,8 @@
 
+from yaw_controller import YawController
+from pid import PID
+from lowpass import LowPassFilter
+
 GAS_DENSITY = 2.858
 ONE_MPH = 0.44704
 
@@ -7,6 +11,17 @@ class Controller(object):
     #def __init__(self, *args, **kwargs):
     def __init__(self, vehicle_mass, fuel_capacity, break_deadband, decel_limit, accel_limit, wheel_radius, wheel_base, steer_ratio, max_lat_accel, max_steer_angle):
         # TODO: Implement
+	
+	# Create Yaw Controller
+	yaw_controller = YawController(wheel_base, steer_ratio, min_speed, max_lat_accel, max_steer_angle)
+
+	# Create PID Controller
+	# kp, ki, kd, min, max
+	pid_controller = PID(?, ?, ?, decel_limit, accel_limit)
+
+	# Create Low Pass Filter
+	# tau, ts
+	low_pass_filter = LowPassFilter(?, ?) 
 	
         pass
 

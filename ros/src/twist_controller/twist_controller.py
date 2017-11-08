d44d2337ea1ab1144b55b358f221c7a7b278c486
jonaschan <jonas.chan@holovis.com>
2017-11-08 07:50:44 -0800

    Modified twist controller

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -10,21 +10,23 @@ ONE_MPH = 0.44704
 class Controller(object):
     #def __init__(self, *args, **kwargs):
     def __init__(self, vehicle_mass, fuel_capacity, break_deadband, decel_limit, accel_limit, wheel_radius, wheel_base, steer_ratio, max_lat_accel, max_steer_angle):
-        # TODO: Implement
+        # Create Yaw Controller
+        self.yaw_controller = YawController(wheel_base, steer_ratio, min_speed, max_lat_accel, max_steer_angle)
+
+        # Create PID Controller
+        # kp, ki, kd, min, max
+        self.kp = ?
+        self.ki = ?
+        self.kd = ?
+        self.speed_controller_pid = PID(self.kp, self.ki, self.kd, decel_limit, accel_limit)
+
+        # Create Low Pass Filter
+        # tau, ts
+        self.tau = ?
+        self.ts = ?
+        self.low_pass_filter = LowPassFilter(self.tau, self.ts) 
 	
-	# Create Yaw Controller
-	yaw_controller = YawController(wheel_base, steer_ratio, min_speed, max_lat_accel, max_steer_angle)
 
-	# Create PID Controller
-	# kp, ki, kd, min, max
-	pid_controller = PID(?, ?, ?, decel_limit, accel_limit)
-
-	# Create Low Pass Filter
-	# tau, ts
-	low_pass_filter = LowPassFilter(?, ?) 
-	
-        pass
-
-    def control(self, *args, **kwargs):
+    def control(self, desired_lin_vel, desired_ang_vel, current_vel, dbw_status):
         # TODO: Change the arg, kwarg list to suit your needs
-        # Return throttle, brake, steer
+        return throttle, brake, steer

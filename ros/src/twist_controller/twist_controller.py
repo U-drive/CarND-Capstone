2568dc3dafa5b7e0416a3d9e979327490f0e2056
Andrew Dunk <drew.dunk@holovis.com>
2017-11-08 14:24:40 -0800

    First attempt at the controller, should provide throttle break and steering. Many comments

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -18,7 +18,7 @@ class Controller(object):
         self.kp = ?
         self.ki = ?
         self.kd = ?
-        self.speed_controller_pid = PID(self.kp, self.ki, self.kd, decel_limit, accel_limit)
+        self.throttle_controller_pid = PID(self.kp, self.ki, self.kd, decel_limit, accel_limit)
 
         # Create Low Pass Filter
         # tau, ts
@@ -26,7 +26,47 @@ class Controller(object):
         self.ts = ?
         self.low_pass_filter = LowPassFilter(self.tau, self.ts) 
 	
+		# Car Total Mass including Fuel
+		# Should we not calculate this outside the constructor to compensate for used fuel?
+		# does fuel_capacity just provide total, or current fuel?
+		self.total_mass = vehicle_mass + (fuel_capacity * GAS_DENSITY)
+		# Breaking force
+		self.breaking_force = total_mass * wheel_radius
+
+		# Init Time
+		self.current_time = rospy.get_time()
+		
 
     def control(self, desired_lin_vel, desired_ang_vel, current_vel, dbw_status):
-        # TODO: Change the arg, kwarg list to suit your needs
+        
+		# Calculate delta time
+		self.delta_time = rospy.get_time() - current_time
+		self.current_time = rospy.get_time()
+
+		# Calculate Throttle Value
+		# Note that throttle values passed to publish should be in the range 0 to 1.
+		self.vel_error = current_vel - desired_lin_vel
+		self.new_acceleration_value = self.throttle_controller_pid.step(self.vel_error, self.delta_time)
+		self.filtered_acceleration_value = self.low_pass_filter.filt(self.new_acceleration_value)
+		
+		if self.filtered_acceleration_value > 0:
+			throttle = self.filtered_acceleration_value
+			# when accelerating make sure we are not breaking
+			brake = 0
+		
+		# Calculate Break Value
+		# Break values passed to publish should be in units of torque (N*m)
+		# The correct values of break can be computed using the desired acceleration, weight
+		# of the vehicle, and wheel radius.
+		if self.filtered_acceleration_value <= 0:
+			# only start breaking if not in the break deadband so that the car can be in a state 
+			# whereby its slowing down without having to apply the breaks for every deceleration
+			if abs(throttle) > break_deadband:
+				brake = abs(throttle) * self.breaking_force
+				# when breaking make sure we are not accelerating
+				throttle = 0
+
+		# Calculate Steer Value
+		steer = self.yaw_controller.get_steering_calculated(desired_lin_vel, desired_ang_vel, current_vel)
+
         return throttle, brake, steer
